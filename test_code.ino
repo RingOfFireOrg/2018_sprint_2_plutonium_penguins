7c8befc728bd66d787403823f6244f13c8a295f7
Mayank Holla <mayank.holla@gmail.com>
2018-12-04 21:20:41 -0500

    all servos working

--- test_code.ino
+++ test_code.ino
@@ -4,21 +4,44 @@ Servo servo_leftB;
 Servo servo_leftF;
 Servo servo_rightB;
 Servo servo_rightF;
+Servo servo_grabR;
+Servo servo_grabL;
 
 void setup() {
     servo_leftB.attach(13);
     servo_leftF.attach(12);
     servo_rightF.attach(11);
     servo_rightB.attach(10);
+    servo_grabL.attach(9);
+    servo_grabR.attach(8);
 }
 
-//void forwarLeftB() {
-//    servo_leftB.write()
-//}
+void wheelsOut() {
+    servo_grabR.write(0); //0 is out
+    servo_grabL.write(180); //180 is out
+}
+
+void wheelsIn() {
+    servo_grabR.write(180); //180 is in
+    servo_grabL.write(0); //0 is in
+}
+
+void wheelsStop() {
+    servo_grabR.write(90); //90 is stop
+    servo_grabL.write(90); //90 is stop
+}
 
 void loop() {
-    servo_leftB.write(0); //forward
-    servo_leftF.write(0); //forward
-    servo_rightB.write(180); //forward
-    servo_rightF.write(180); //forward
+    //servo_leftB.write(0); //forward is 0
+    //servo_leftF.write(0); //forward is 0
+    //servo_rightB.write(180); //forward is 180
+    //servo_rightF.write(180); //forward is 180
+    wheelsOut();
+    delay(1000);
+    wheelsStop();
+    delay(1000);
+    wheelsIn();
+    delay(1000);
+    wheelsStop();
+    delay(1000);
 }
